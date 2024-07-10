c26756d3dc152f621ec69f9d6d4304940f2a54ca
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-10 09:59:24 +0900

    svelte-work - first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Svelte Work
+Svelte + Vite + Flowbite + Tailwind
+
+# Creating Project
+The project is created by [flowbite-svelte](https://github.com/themesberg/flowbite-svelte)
---
+++ app/src/App.svelte
@@ -0,0 +1,14 @@
+
+<main>
+  <h1>Vite + Svelte</h1>
+  <div class="mt-4">
+    <Alert>
+      <span class="font-medium">Info alert!</span>
+    </Alert>
+  </div>
+</main>
+
+
+<script>import "./app.css";
+import { Alert } from 'flowbite-svelte';
+</script>
