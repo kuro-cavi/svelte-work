9ba2064e5d0b9f747a235c432b32a8536bd084b4
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-10 10:39:42 +0900

    remove css

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,14 +1,15 @@
-
 <main>
-  <h1>Vite + Svelte</h1>
-  <div class="mt-4">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-    </Alert>
+  <div class="text-center">
+    <h1>Vite + Svelte</h1>
+    <div class="mt-4">
+      <Alert>
+        <span class="font-medium">Info alert!</span>
+      </Alert>
+    </div>
   </div>
 </main>
 
-
-<script>import "./app.css";
-import { Alert } from 'flowbite-svelte';
+<script>
+  import "./app.css";
+  import { Alert } from "flowbite-svelte";
 </script>
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -4,6 +4,8 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}", './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
 
+  darkMode: 'class',
+
   theme: {
     extend: {
       colors: {
