f8ab7c15c9ae95de75074bc95acaefaf3bbd9a9c
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-16 09:24:53 +0900

    test library

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {svelteTesting} from '@testing-library/svelte/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: '0.0.0.0'
   },
-  plugins: [svelte()],
+  plugins: [svelte(), svelteTesting()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.js'],
+  },
 })
