6e556b2925f4e53007875303e4dd6564966e4166
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-16 19:10:30 +0900

    introduction - key block

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,19 +1,3 @@
-<!-- <script>
-  import "./app.css";
-  import { Alert } from "flowbite-svelte";
-</script>
-
-<main>
-  <div class="text-center">
-    <h1>Vite + Svelte</h1>
-    <div class="mt-4">
-      <Alert>
-        <span class="font-medium">Info alert!</span>
-      </Alert>
-    </div>
-  </div>
-</main> -->
-
 <script>
   import "./app.css";
   import { Alert } from "flowbite-svelte";
---
+++ app/src/lib/introduction/logic-blocks/key-block.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { Button, Card } from "flowbite-svelte";
+
+  let count = 0;
+
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<Card>
+  <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+    Key Block Render
+  </h5>
+  <div class="mb-2 h-7">
+    {#key count}
+      <!-- When used around components, this will cause them to be reinstantiated and reinitialised. -->
+      <div in:fade>
+        {count}
+      </div>
+    {/key}
+  </div>
+  <div>
+    <Button on:click={increment}>Increment</Button>
+  </div>
+</Card>
