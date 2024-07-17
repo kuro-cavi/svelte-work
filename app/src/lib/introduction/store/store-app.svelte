61af760b3837d9286fed6030acadbed4d67b2e4f
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-17 13:38:09 +0900

    store

---
+++ app/src/lib/introduction/store/store-app.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Button, Card } from "flowbite-svelte";
+  import { count } from "./store";
+</script>
+
+<Card>
+  <p class="mb-4">count: {$count}</p>
+  <div class="flex gap-4 justify-center items-center">
+    <Button on:click={count.increment}>+</Button>
+    <Button on:click={count.decrement}>-</Button>
+    <Button on:click={count.reset}>reset</Button>
+  </div>
+</Card>
