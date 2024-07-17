8bb1aa39e32d57f23207d27ec3de901edd7f4c82
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-17 13:37:39 +0900

    svelte tick

---
+++ app/src/lib/introduction/svelte-tick/tick-handle.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Button, Card } from "flowbite-svelte";
+  import { tick } from "svelte";
+
+  let count = 0;
+
+  const increment = async () => {
+    count += 1;
+    await tick();
+    // Code to be executed after DOM is updated
+    console.log("DOM has been updated with new count:", count);
+  };
+</script>
+
+<Card>
+  <Button on:click={increment}>Increment</Button>
+  <p>count: {count}</p>
+</Card>
