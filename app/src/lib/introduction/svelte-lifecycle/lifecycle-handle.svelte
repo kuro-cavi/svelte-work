f1aedc271c305a82a7c00fcd457a3a251ecbbc8d
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-17 13:37:16 +0900

    svelte lifecycle

---
+++ app/src/lib/introduction/svelte-lifecycle/lifecycle-handle.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { onMount, beforeUpdate, afterUpdate, onDestroy } from "svelte";
+
+  onMount(() => {
+    console.log('the component has mounted');
+
+    return () => {
+      console.log('the component has dismounting');
+    };
+  });
+
+  beforeUpdate(() => {
+    console.log('the component is about to update');
+  });
+
+  afterUpdate(() => {
+    console.log('the component just updated');
+
+  });
+
+  onDestroy(() => {
+    console.log('the component is being destroyed');
+  });
+</script>
