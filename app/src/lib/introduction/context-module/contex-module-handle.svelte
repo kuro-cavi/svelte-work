5ab932841d6cc5a4f32e0c114e617aaadbeb0a4a
kuro-cavi <f.w.kurogi@gmail.com>
2024-07-18 16:17:23 +0900

    context module

---
+++ app/src/lib/introduction/context-module/contex-module-handle.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Button, Card } from "flowbite-svelte";
+  import Shared, { getTotal } from "../context-module/shared.svelte";
+
+  let total = getTotal();
+
+  const handle = () => {
+    total = getTotal();
+  };
+</script>
+
+<Card>
+  <div class="flex flex-col space-y-2">
+    <Button on:click={handle}>Total Count: {total}</Button>
+    <Shared />
+    <Shared />
+    <Shared />
+  </div>
+</Card>
---
+++ app/src/lib/introduction/context-module/shared.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+  // not trigger rendering
+  let total: number = 0;
+
+  export const getTotal = () => {
+    return total;
+  };
+</script>
+
+<script lang="ts">
+  import { Button } from "flowbite-svelte";
+
+  let count: number = 0;
+  const handle = () => {
+    count++;
+    total++;
+  };
+</script>
+
+<Button on:click={handle}>Shared Count: {count}</Button>
